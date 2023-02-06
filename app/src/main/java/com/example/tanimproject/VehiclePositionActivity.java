ff8129decc0673b35a0d7755f47a36de9e812d36
Liakot-Ali <liakotali2k18@gmail.com>
2023-02-06 18:57:56 +0600

    Creating and desiging realtime monitoring page

---
+++ app/src/main/java/com/example/tanimproject/VehiclePositionActivity.java
@@ -0,0 +1,14 @@
+package com.example.tanimproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VehiclePositionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_position);
+    }
+}
