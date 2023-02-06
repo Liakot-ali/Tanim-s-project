30b5939351d98b10ba01312282c1b8985c6f8c7d
Liakot-Ali <liakotali2k18@gmail.com>
2023-02-06 19:03:34 +0600

    Linking Realtime monitoring design page with java file

--- app/src/main/java/com/example/tanimproject/HomeActivity.java
+++ app/src/main/java/com/example/tanimproject/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.tanimproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -34,7 +35,8 @@ public class HomeActivity extends AppCompatActivity {
         rtMonitoring.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(HomeActivity.this, "Under Construction", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(HomeActivity.this, "Under Construction", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(HomeActivity.this, RTMonitoringActivity.class));
             }
         });
 
--- app/src/main/java/com/example/tanimproject/RTMonitoringActivity.java
+++ app/src/main/java/com/example/tanimproject/RTMonitoringActivity.java
@@ -2,13 +2,52 @@ package com.example.tanimproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 public class RTMonitoringActivity extends AppCompatActivity {
+
+    Button front, rear, sentAlert;
+    VideoView realtimeVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rtmonitoring);
+
+        InitializeAll();
+    }
+
+    private void InitializeAll() {
+
+        front = findViewById(R.id.rtFront);
+        rear = findViewById(R.id.rtRear);
+        sentAlert = findViewById(R.id.rtSentAlert);
+
+        realtimeVideo = findViewById(R.id.rtVideo);
+
+        front.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RTMonitoringActivity.this, "Under Construction", Toast.LENGTH_SHORT).show();
+            }
+        });
+        rear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RTMonitoringActivity.this, "Under Construction", Toast.LENGTH_SHORT).show();
+            }
+        });
+        sentAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(RTMonitoringActivity.this, "Under Construction", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
