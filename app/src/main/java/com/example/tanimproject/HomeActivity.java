cb748cf4a486cb9a7bff41e23b2eed13c57b2b12
Liakot-Ali <liakotali2k18@gmail.com>
2023-02-06 18:42:30 +0600

    Linking home page with java file

--- app/src/main/java/com/example/tanimproject/HomeActivity.java
+++ app/src/main/java/com/example/tanimproject/HomeActivity.java
@@ -3,12 +3,40 @@ package com.example.tanimproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TextView dailyRecord, position, rtMonitoring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        dailyRecord = findViewById(R.id.homeDailyRecord);
+        position = findViewById(R.id.homeVehiclePosition);
+        rtMonitoring = findViewById(R.id.homeRTMonitoring);
+
+        dailyRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(HomeActivity.this, "Under Construction", Toast.LENGTH_SHORT).show();
+            }
+        });
+        position.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(HomeActivity.this, "Under Construction", Toast.LENGTH_SHORT).show();
+            }
+        });
+        rtMonitoring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(HomeActivity.this, "Under Construction", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
